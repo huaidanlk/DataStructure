700d0b0d64205048e3a843717a0517778db7b2aa
huaidan <1208529858@qq.com>
2021-06-30 21:49:00 +0800

    前缀节点

--- src/binaryTree/BinaryTreeTest.java
+++ src/binaryTree/BinaryTreeTest.java
@@ -2,14 +2,16 @@ package binaryTree;
 
 public class BinaryTreeTest {
     public static void main(String[] args) {
-        int[] array = new int[]{7, 4, 2, 1, 3, 5, 9, 8, 11, 10, 12};
+//        int[] array = new int[]{7, 4, 2, 1, 3, 5, 9, 8, 11, 10, 12};
+        int[] array = new int[]{7, 4, 9, 2, 1};
+
         BinarySearchTree mTree = new BinarySearchTree();
         for (int i = 0; i < array.length; i++) {
             mTree.add(array[i]);
         }
 
-        System.out.println(mTree.height1(mTree.root));
+//        System.out.println(mTree.height1(mTree.root));
 //        mTree.levelTraversal(mTree.root);
-
+       System.out.println(mTree.isComplete(mTree.root));
     }
 }
--- src/binaryTree/TreeNode.java
+++ src/binaryTree/TreeNode.java
@@ -6,12 +6,22 @@ public class TreeNode {
     TreeNode right;
     TreeNode parent;
 
-    public TreeNode(int value) {
+    public TreeNode(int value, TreeNode parent) {
         this.value = value;
+        this.parent = parent;
     }
 
     @Override
     public String toString() {
+
         return String.valueOf(value);
+    }
+
+    boolean hasTwoChildren() {
+        return left != null && right != null;
+    }
+
+    boolean isLeaf() {
+        return left == null && right == null;
     }
 }
